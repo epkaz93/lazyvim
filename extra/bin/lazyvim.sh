3af3746a812f7bb3eeac37e1011499f54b330131
epkaz93 <thomaswilmot@gmail.com>
2025-01-22 13:10:33 +1100

    Move and update lazyvim launch script

---
+++ extra/bin/lazyvim.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# Copy to:
+#   /usr/local/bin/lazyvim
+
+export NVIM_APPNAME=lazyvim
+exec nvim "$@"
--- extra/lazyvim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Copy to:
-#   /usr/local/bin/lazyvim
-
-NVIM_APPNAME=lazyvim nvim "$@"
