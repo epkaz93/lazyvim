fdaa3cb762d33e5dc3aceb9cd03c2846627d29b6
epkaz93 <thomaswilmot@gmail.com>
2025-02-12 12:37:34 +1100

    Added snacks explorer and removed neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     name = "neo-tree",
+    enabled = false,
     opts = {
       filesystem = {
         follow_current_file = { enabled = false },
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  opts = {
+    explorer = {
+      enabled = true,
+      replace_netrw = true,
+    },
+  },
+}
