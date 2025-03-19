35e707802ab3512982450614b65e1229189da965
epkaz93 <thomaswilmot@gmail.com>
2025-03-19 13:41:31 +1100

    Update minimap to be split instead of float
    Solves issue where long lines get hidden by minimap

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -3,5 +3,10 @@ return {
     "Isrothy/neominimap.nvim",
     name = "neominimap",
     enabled = true,
+    init = function()
+      vim.g.neominimap = {
+        layout = "split",
+      }
+    end,
   },
 }
